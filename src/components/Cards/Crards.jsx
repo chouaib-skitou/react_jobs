536557fa3e2bde6339247f6f8c6d0c9875fa04fb
Chouaib SKITOU <chouaib.skt@gmail.com>
2024-11-24 15:29:21 +0100

    setup the project and add template

---
+++ src/components/Cards/Crards.jsx
@@ -0,0 +1,24 @@
+export const PrimaryCard = () => {
+    return (
+      <div className="card primary">
+        <h3>Primary Card</h3>
+      </div>
+    );
+  };
+  
+  export const SecondaryCard = () => {
+    return (
+      <div className="card secondary">
+        <h3>Secondary Card</h3>
+      </div>
+    );
+  };
+  
+  export const TertiaryCard = () => {
+    return (
+      <div className="card tertiary">
+        <h3>Tertiary Card</h3>
+      </div>
+    );
+  };
+  
